57655872a9a49ecf02cebd964e0380b5449594c4
jhonatancasale <jhonatan@jhonatancasale.com>
2017-06-23 01:52:07 -0300

    Add some examples

---
+++ Part III/libs, Frameworks and afins/Logging/file.output/logging.pizza.py
@@ -0,0 +1,27 @@
+"""from: https://www.digitalocean.com/community/tutorials/how-to-use-logging-in-python-3"""
+
+import logging
+
+logging.basicConfig(filename='test.log', level=logging.DEBUG)
+
+
+class Pizza():
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+        logging.debug("Pizza created: {} (${})".format(self.name, self.price))
+
+    def make(self, quantity=1):
+        logging.debug("Made {} {} pizza(s)".format(quantity, self.name))
+
+    def eat(self, quantity=1):
+        logging.debug("Ate {} pizza(s)".format(quantity, self.name))
+
+
+pizza_01 = Pizza("artichoke", 18)
+pizza_01.make(5)
+pizza_01.eat(4)
+
+pizza_02 = Pizza("margherita", 16)
+pizza_02.make(2)
+pizza_02.eat(2)
